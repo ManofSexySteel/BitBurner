d8943134b7edd04099bf299cec3f563895b1321a
ManofSexySteel <166028128+ManofSexySteel@users.noreply.github.com>
2024-04-04 03:43:32 -1000

    Create bin.gr.js

---
+++ bin.gr.js
@@ -0,0 +1,11 @@
+//bin.gr.js
+//grows the servers money
+
+/** @param {NS} ns **/
+export async function main(ns) {
+	let target = ns.args[0];
+	let repeat = ns.args[1];
+	do {
+		await ns.grow(target)
+	} while (repeat)
+}
